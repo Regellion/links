e8904be0176a84d296dd467417b5c854956e5d13
Regellion <55495658+Regellion@users.noreply.github.com>
2020-06-26 12:27:48 +0300

    Example code

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.*;
+
+public class Main {
+    private static Set<String> uniqueURL = new HashSet<>();
+    private static final String siteURL = "https://skillbox.ru/";
+
+    public static void main(String[] args) {
+        getLinks(siteURL, "");
+        //uniqueURL.forEach(System.out::println);
+        System.out.println(uniqueURL.size());
+    }
+
+    private static void getLinks(String pageUrl, String tab) {
+        // добавляем введеную ссылку
+        uniqueURL.add(pageUrl);
+        System.out.println(tab + pageUrl);
+        // Если эта ссылка ведет на файл то прерываем метод
+        if (!pageUrl.endsWith("/")) {
+            return;
+        }
+        Document document = null;
+        try {
+            Thread.sleep(200);
+            document = Jsoup.connect(pageUrl).maxBodySize(0).get();
+        } catch (Exception ignored) {
+            // потом логировать
+        }
+        if (document == null) {
+            return;
+        }
+        Elements links = document.select("a");
+
+        if (links.isEmpty()) {
+            return;
+        }
+
+
+        links.stream().map(link -> link.attr("href")).forEachOrdered(url -> {
+            if (!url.startsWith(siteURL)) {
+                return;
+            }
+            if (url.contains("#")) {
+                return;
+            }
+            if (uniqueURL.contains(url)) {
+                return;
+            }
+
+            uniqueURL.add(url);
+            String newTab = tab + "\t";
+            getLinks(url, newTab);
+        });
+    }
+}
