a5df96b09044e7062815dfca770a1f066b402e52
Regellion <55495658+Regellion@users.noreply.github.com>
2020-07-04 22:44:25 +0300

    Add files via upload

--- src/main/java/WalkLink.java
+++ src/main/java/WalkLink.java
@@ -2,30 +2,38 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveAction;
 
 public class WalkLink extends RecursiveAction {
     private String url;
     private static Set<String> uniqueURL = Collections.synchronizedSet(new HashSet<>());
-    private static final String siteURL = "https://skillbox.ru/";
+    private String siteURL;
     private String tab;
+    private static List<String> linksList = Collections.synchronizedList(new ArrayList<>());
 
-    WalkLink(String url, String tab){
+    WalkLink(String url, String tab, String siteURL){
         this.url = url;
         this.tab = tab;
+        this.siteURL = siteURL;
     }
+
     @Override
     protected void compute() {
+
         if(uniqueURL.contains(url)){
             return;
         }
-        // Если ссылка уникальная, печатаем. потом можно будет записывать в файл
-        System.out.println(tab + url);
+        // Если ссылка уникальная пишем в файл
+        linksList.add(tab + url);
+        //System.out.println(tab + url);
         // Добавляем в сет
         uniqueURL.add(url);
-
         try{
+
             //Пробуем подключиться к ссылки
             Document document = Jsoup.connect(url)
                     .maxBodySize(0)
@@ -44,7 +52,7 @@ public class WalkLink extends RecursiveAction {
 
             List<WalkLink> subTasks = new LinkedList<>();
 
-            links.stream().map(link -> link.attr("abs:href"))
+            links.stream().map(link -> link.attr("href"))
                     .filter(url-> url.startsWith(siteURL))
                     .filter(url-> !url.contains("#"))
                     .map(url-> {
@@ -57,21 +65,24 @@ public class WalkLink extends RecursiveAction {
                     .forEachOrdered(url-> {
                         //Все ссылки прощедшие фильтры делаем подзадачами
                         String newTab = tab + "\t";
-                        WalkLink task = new WalkLink(url, newTab);
+                        WalkLink task = new WalkLink(url, newTab, siteURL);
                         task.fork();
                         subTasks.add(task);
-                        //TODO Метод работает верно, собирает ровно столько же ссылок сколько и в однопоточном режиме,
-                        // но происходит Конкуренция потоков. Из за этого лесенка не получается красивой. Если
-                        // здесь применить метод join(), то тогда все отображается отлично, но по сути, приложение так же становиться однопоточным.
-                        // Как всего этого избежать пока не пойму.
                     });
 
-            subTasks.forEach(task-> {
-                // Собираем результат всех подзадач
-                task.join();
-            });
+            // Собираем результат всех подзадач
+            subTasks.forEach(ForkJoinTask::join);
 
-        }catch (Exception ignored){}
+        }catch (Exception ex){
+            System.out.println("Connection is failed. URL: " + url + " " + ex.getMessage());
+        }
+    }
 
+    void writer(String filePath) throws IOException {
+        FileWriter fileWriter = new FileWriter(filePath);
+        StringBuffer sb = new StringBuffer();
+        linksList.forEach(url-> sb.append(url).append("\n"));
+        fileWriter.write(String.valueOf(sb));
+        fileWriter.close();
     }
 }
